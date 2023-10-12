f71efddeba9d3942d7ee6ac36bb1a31be520e343
MSI <chathurabhashitha01@gmail.com>
2023-10-12 23:12:59 +0530

    plase order form validation creation

--- Assignment/JS/InMemoryPOS/controllers/validation/PlaceOrderValidation.js
+++ Assignment/JS/InMemoryPOS/controllers/validation/PlaceOrderValidation.js
@@ -1,25 +1,49 @@
 const ORDER_ID_REGEX = /^(OR00-)[0-9]{3}$/;
 
-$("#txtOrderId").keyup(function (e) {
+let placeOrderArray=new Array();
+placeOrderArray.push({field:$("#txtOrderId"), regEx:ORDER_ID_REGEX});
+placeOrderArray.push({field:$("#cmbCustomer"), regEx:CUS_ID_REGEX});
+placeOrderArray.push({field:$("#txtCustomerName"), regEx:CUS_NAME_REGEX});
+placeOrderArray.push({field:$("#txtCustomerAddress"), regEx:CUS_ADDRESS_REGEX});
+placeOrderArray.push({field:$("#txtCustomerSalary"), regEx:CUS_SALARY_REGEX});
+placeOrderArray.push({field:$("#cmbItemCode"), regEx:ITEM_CODE_REGEX});
+placeOrderArray.push({field:$("#txtGetItemName"), regEx:ITEM_NAME_REGEX});
+placeOrderArray.push({field:$("#txtGetItemPrice"), regEx:ITEM_PRICE_REGEX});
+placeOrderArray.push({field:$("#txtGetQtyOnHand"), regEx:ITEM_QTY_REGEX});
 
-    let value = $("#txtOrderId").val();
 
-    if (value.length == 0) {
-        $("#txt").css('border', '1px solid #ced4da');
 
-    } else {
-        let res = ORDER_ID_REGEX.test(value);
-        if (res) {
-            $("#txtOrderId").css('border', '2px solid green');
-        } else {
-            $("#txtOrderId").css('border', '2px solid red');
-        }
+$("#txtOrderId,#cmbCustomer,#txtCustomerName,#txtCustomerAddress,#txtCustomerSalary,#cmbItemCode,#txtGetItemName,#txtGetItemPrice,#txtGetQtyOnHand").on("keydown keyup", function (e) {
+    let indexNo = placeOrderArray.indexOf(placeOrderArray.find((c) => c.field.attr("id") == e.target.id));
+
+    if (e.key == "Tab") {
+        e.preventDefault();
     }
 
-});
-//const inputChangeEvent = new Event('input', { bubbles: true });
+    checkValidation(placeOrderArray[indexNo]);
 
-// function setAndTriggerValue($element, value) {
-//     $element.val(value);
-//     $element[0].dispatchEvent(inputChangeEvent);
-// }
+});
+function checkValidation(object) {
+    if (object.regEx.test(object.field.val())) {
+        setBorder(true, object);
+        return true;
+    }
+    setBorder(false, object)
+    return false;
+}
+function setBorder(bol,object){
+    if(bol){
+        if (object.field.val().length >= 1) {
+            object.field.css("border", "2px solid green");
+        } else {
+            object.field.css("border", "1px solid #ced4da");
+        }
+    }
+    else {
+        if (object.field.val().length >= 1) {
+            object.field.css("border", "2px solid red");
+        } else {
+            object.field.css("border", "1px solid #ced4da");
+        }
+    }
+}

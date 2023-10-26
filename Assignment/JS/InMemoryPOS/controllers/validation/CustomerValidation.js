831f4e379d002adb13c6d9d5eab837f6dec1791e
MSI <chathurabhashitha01@gmail.com>
2023-10-26 18:50:50 +0530

    portfolio update

--- Assignment/JS/InMemoryPOS/controllers/validation/CustomerValidation.js
+++ Assignment/JS/InMemoryPOS/controllers/validation/CustomerValidation.js
@@ -1,5 +1,5 @@
 const CUS_ID_REGEX = /^(C00-)[0-9]{3}$/;
-const CUS_NAME_REGEX = /^[A-Za-z ]{5,}$/;
+const CUS_NAME_REGEX = /^[A-Za-z ]{}$/;
 const CUS_ADDRESS_REGEX = /^[A-Za-z0-9 ]{5,}$/;
 const CUS_SALARY_REGEX = /^[0-9]{2,}([.][0-9]{2})?$/;
 

efbd72c872bd900bee51a0937cbd963f347964cf
MSI <chathurabhashitha01@gmail.com>
2023-10-28 18:29:29 +0530

    inMemoryPOS updated

--- Assignment/JS/InMemoryPOS/controllers/validation/CustomerValidation.js
+++ Assignment/JS/InMemoryPOS/controllers/validation/CustomerValidation.js
@@ -1,5 +1,5 @@
 const CUS_ID_REGEX = /^(C00-)[0-9]{3}$/;
-const CUS_NAME_REGEX = /^[A-Za-z ]{}$/;
+const CUS_NAME_REGEX = /^[A-Za-z ]{2,}$/;
 const CUS_ADDRESS_REGEX = /^[A-Za-z0-9 ]{5,}$/;
 const CUS_SALARY_REGEX = /^[0-9]{2,}([.][0-9]{2})?$/;
 
